29e34f34ed7676a4d4803622dee09d16b529a418
PotomaceCycle <35033158+PotomaceCycle@users.noreply.github.com>
2018-01-02 08:59:00 -0500

    Switched to new IDE
    Bunch of random stuff added to the project folder as a result

--- Prototype1-2-18/Prototype1-2-18.ino
+++ Prototype1-2-18/Prototype1-2-18.ino
@@ -1,31 +1,37 @@
+int amperageThreshold; //At what amperage we want our shredder to back that ass up
+
 void setup() {
-  // put your setup code here, to run once:
+	// put your setup code here, to run once:
+	//initialize some starting variables that wont change (yet)
+
+	amperageThreshold = 75;
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
 
+int getAmperageLoad() {
+	int amperageLoad;
+	//get input from sensor pins, return it in int
 
-int getAmperageLoad()
-{
-  //get input from sensor pins, return it in int
+	return amperageLoad;
 }
 
-void forward()
-{
-  //move it forward
+void forward() {
+	//move it forward
 }
 
-void backward()
-{
-  //move it backwards
+void backward() {
+	//move it backwards
 }
 
-void senseOverload()
-{
-  //decides when an overload has occured, reverses motor for certain seconds.
+void senseOverload() {
+	//decides when an overload has occured, reverses motor for certain seconds.
+	if (getAmperageLoad() >= amperageThreshold) {
+
+	}
+
 }
 
