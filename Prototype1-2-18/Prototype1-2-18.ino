47cf8d091cb48ecda0a351fa0f54ef32aa4ec992
PotomaceCycle <35033158+PotomaceCycle@users.noreply.github.com>
2018-01-02 08:31:58 -0500

    Added very basic frame
    Needs more thought, as well

--- Prototype1-2-18/Prototype1-2-18.ino
+++ Prototype1-2-18/Prototype1-2-18.ino
@@ -7,3 +7,25 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+int getAmperageLoad()
+{
+  //get input from sensor pins, return it in int
+}
+
+void forward()
+{
+  //move it forward
+}
+
+void backward()
+{
+  //move it backwards
+}
+
+void senseOverload()
+{
+  //decides when an overload has occured, reverses motor for certain seconds.
+}
+
